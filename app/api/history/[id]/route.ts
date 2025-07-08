a8b3d21f8b5ddb553c93e00b63af50287f8bb5d3
luis.condori <luis.condori@produccion.gob.bo>
2025-07-07 20:11:22 -0400

    Nueva version de gestor de tareas

--- app/api/history/[id]/route.ts
+++ app/api/history/[id]/route.ts
@@ -1,54 +1,54 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 import { validateToken } from '@/utils/validateToken'
 import { TaskHistoryService } from '../../tasks/TaskHistoryService';
 
-export async function GET(
-  request: NextRequest,
-  { params }: { params: { id: string } }
-) {
-  const session = await validateToken();
+export const GET = async (request: Request, { params }: { params: { id: number } }) => {
+
+  const session = await validateToken()
 
   if (!session) {
-    return NextResponse.json({ error: 'No autorizado.' }, { status: 401 });
+    return NextResponse.json({ error: 'No autorizado.' }, { status: 401 })
   }
 
   try {
-    const { id } = params;
+    const { id } = await params;
 
     if (!id) {
       return NextResponse.json({ message: 'ID de tarea es necesario' }, { status: 400 });
     }
 
-    const task = await TaskHistoryService.getAllTaskHistory(parseInt(id, 10));
+    const task = await TaskHistoryService.getAllTaskHistory(id);
 
-    return NextResponse.json(task, { status: 200 });
+    return NextResponse.json(task, { status: 200 }); 
 
   } catch (error) {
     return NextResponse.json({ message: 'Error interno del servidor.' }, { status: 500 });
   }
 }
 
-export async function DELETE(
-  request: NextRequest,
-  { params }: { params: { id: string } }
-) {
-  const session = await validateToken();
+export const DELETE = async (request: Request, { params }: { params: { id: string } }) => {
+
+  const session = await validateToken()
 
   if (!session) {
-    return NextResponse.json({ error: 'No autorizado.' }, { status: 401 });
+    return NextResponse.json({ error: 'No autorizado.' }, { status: 401 })
   }
 
   try {
     const { searchParams } = new URL(request.url);
     const id_history = searchParams.get('id_history');
-
+    
     if (id_history) {
-      await TaskHistoryService.deleteSingleTaskHistory(parseInt(id_history, 10));
+
+      await TaskHistoryService.deleteSingleTaskHistory(parseInt(id_history));
       return NextResponse.json({ success: true, message: 'Elemento del historial eliminado correctamente' });
+
     } else {
-      const id_user = parseInt(params.id, 10);
+      
+      const id_user = parseInt(params.id);
       await TaskHistoryService.deleteAllTaskHistory(id_user);
       return NextResponse.json({ success: true, message: 'Historial completo eliminado correctamente' });
+
     }
 
   } catch (error) {
